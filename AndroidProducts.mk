6da26d8ad0f0a60d77f79ed10bedabfb65481d50
delawcharles <delawcharles@gmail.com>
2020-11-15 05:45:15 +0000

    odessa: Add more brand identifiers
    Better structuring of builds, necessary for a better understanding
    
    Signed-off-by: Charles Delaware <delawcharles@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -16,3 +16,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_odessa.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_odessa-eng \
+    lineage_odessa-user \
+    lineage_odessa-userdebug
--- lineage_odessa.mk
+++ lineage_odessa.mk
@@ -24,16 +24,21 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from odessa device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-PRODUCT_BRAND := motorola
+# Brand identifiers
 PRODUCT_DEVICE := odessa
-PRODUCT_MANUFACTURER := motorola
 PRODUCT_NAME := lineage_odessa
-PRODUCT_MODEL := moto g(9) plus
+PRODUCT_BRAND := motorola
+PRODUCT_MANUFACTURER := motorola
+PRODUCT_MODEL := Moto G9 Plus
+PRODUCT_RELEASE_NAME := odessa
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 TARGET_VENDOR := motorola
 TARGET_VENDOR_PRODUCT_NAME := odessa
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="odessa_retail-user 10 QPA30.19-Q3-32-39-1 5ca099 release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=odessa \
+    PRIVATE_BUILD_DESC="odessa_retail-user 10 QPA30.19-Q3-32-39-1 5ca099 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := motorola/odessa_retail/odessa:10/QPA30.19-Q3-32-39-1/5ca099:user/release-keys
